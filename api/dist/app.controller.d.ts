03e3670cf2aae4c0834fe4f51ba854cf4327c92d
Cody Mumford <cody-mumford9106@hotmail.com>
2022-03-11 13:12:35 -0600

    remove dist from git

--- api/dist/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { GameService } from './game.service';
-import { NamedAPIResourceList } from 'pokenode-ts';
-export declare class AppController {
-    private readonly gameService;
-    constructor(gameService: GameService);
-    getVersions(): Promise<NamedAPIResourceList>;
-}
--- api/dist/game.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { NamedAPIResourceList } from 'pokenode-ts';
-export declare class GameService {
-    private client;
-    constructor();
-    getVersions(): Promise<NamedAPIResourceList>;
-}
